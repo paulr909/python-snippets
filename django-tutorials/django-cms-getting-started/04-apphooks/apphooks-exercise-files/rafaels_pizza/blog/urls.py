5d8a682d4eb8b95be2fd56ba4b85cab833dd0ebc
paulrogers <paulr909@proton.me>
2023-08-24 16:57:28 +0100

    rm django v1.8 project

--- django-tutorials/django-cms-getting-started/04-apphooks/apphooks-exercise-files/rafaels_pizza/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.blog),
-    url(r'^category/(?P<c_id>[\-\w]+)/$', views.category),
-    url(r'^post/(?P<p_id>[\-\w]+)/$', views.post)
-]
