9f016557f6d8084b497e9cec81bd3d0a7205e61e
paulrogers <paulr909@proton.me>
2023-08-24 18:08:03 +0100

    edit folders

--- python-tutorials/Up and Running with Raspberry Pi-Exercise Files/Exercise Files/sn754410_B_driver.py
+++ python-tutorials/Up and Running with Raspberry Pi-Exercise Files/Exercise Files/sn754410_B_driver.py
@@ -1,48 +1,51 @@
-
 # driver program for sn754410
 
-from time import sleep #We will need to sleep the code at points
-import RPi.GPIO as GPIO #Import the GPIO library as GPIO
+from time import sleep  # We will need to sleep the code at points
+import RPi.GPIO as GPIO  # Import the GPIO library as GPIO
 import signal
 import sys
 
-#Setup GPIO
-GPIO.setmode(GPIO.BOARD) 
-GPIO.setwarnings(False) # Ignore any errors
+# Setup GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)  # Ignore any errors
 
 sleeptime = 6
 
-GPIO.setup(11,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 
 
 def forward():
-        print "forward"
-        GPIO.output(11,1)
-        GPIO.output(13,0)
+    print("forward")
+    GPIO.output(11, 1)
+    GPIO.output(13, 0)
+
 
 def backwards():
-        print "backwards"
-        GPIO.output(11,0)
-        GPIO.output(13,1)
-        
+    print("backwards")
+    GPIO.output(11, 0)
+    GPIO.output(13, 1)
+
+
 def stop():
-        print "stop"
-        GPIO.output(11,0)
-        GPIO.output(13,0)
+    print("stop")
+    GPIO.output(11, 0)
+    GPIO.output(13, 0)
 
-def breakhandler(signum,frame):
-        print "goodbye"
-        stop()
-        GPIO.cleanup
-        sys.exit()
 
-signal.signal(signal.SIGINT,breakhandler)
+def breakhandler(signum, frame):
+    print("goodbye")
+    stop()
+    GPIO.cleanup
+    sys.exit()
+
+
+signal.signal(signal.SIGINT, breakhandler)
 
 while True:
-        forward()
-        sleep(sleeptime)
-        stop()
-        backwards()
-        sleep(sleeptime)
-        stop()
+    forward()
+    sleep(sleeptime)
+    stop()
+    backwards()
+    sleep(sleeptime)
+    stop()
