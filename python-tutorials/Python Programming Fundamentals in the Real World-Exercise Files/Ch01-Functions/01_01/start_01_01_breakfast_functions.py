2c68af2d99316146ca8d17322ca3b6e51bc000ef
paulrogers <paulr909@proton.me>
2023-08-26 15:49:05 +0100

    rm start files

--- python-tutorials/Python Programming Fundamentals in the Real World-Exercise Files/Ch01-Functions/01_01/start_01_01_breakfast_functions.py
+++
@@ -1,8 +0,0 @@
-""" A Functional Breakfast """
-
-print('Mixing the ingredients')
-print('Pouring the mixture into a frying pan')
-print('Cooking the first side')
-print('Flipping it!')
-print('Cooking the other side')
-omelette = 'a tasty omelette'
