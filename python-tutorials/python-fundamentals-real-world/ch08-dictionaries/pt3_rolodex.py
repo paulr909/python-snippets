888c47f4a4b3301b5559390eeebb4703bcaee392
paulrogers <paulr909@proton.me>
2023-08-26 18:15:37 +0100

    refactor ch08

--- python-tutorials/python-fundamentals-real-world/ch08-dictionaries/pt3_rolodex.py
+++ python-tutorials/python-fundamentals-real-world/ch08-dictionaries/pt3_rolodex.py
@@ -1,15 +1,17 @@
 """ A Rolodex Full of Friends """
 
-# dictionary of name/number pairs
-rolodex = {'Aaron': 5556069,
-           'Bill': 5559824,
-           'Dad': 5552603,
-           'David': 5558331,
-           'Dillon': 5553538,
-           'Jim': 5555547,
-           'Mom': 5552603,
-           'Olivia': 5556397,
-           'Verne': 5555309}
+# dictionary of key/value pairs
+rolodex = {
+    "Aaron": 5556069,
+    "Bill": 5559824,
+    "Dad": 5552603,
+    "David": 5558331,
+    "Dillon": 5553538,
+    "Jim": 5555547,
+    "Mom": 5552603,
+    "Olivia": 5556397,
+    "Verne": 5555309,
+}
 
 
 def caller_id(lookup_number):
