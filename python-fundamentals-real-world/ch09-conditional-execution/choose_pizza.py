1b19eb475de45734c8f53db2dbcc15cb6efa04d7
paulrogers <paulr909@proton.me>
2023-08-26 18:44:46 +0100

    format files

--- python-fundamentals-real-world/ch09-conditional-execution/choose_pizza.py
+++ python-fundamentals-real-world/ch09-conditional-execution/choose_pizza.py
@@ -1,14 +1,14 @@
 """ Ordering A Pizza That Verne Can Eat """
 
 # things that Verne does not eat
-diet_restrictions = {'meat', 'cheese'}
+diet_restrictions = {"meat", "cheese"}
 
-# decide which pizza to order      
-if 'meat' and 'cheese' in diet_restrictions:
-    print('Get a vegan pizza.')
+# decide which pizza to order
+if "meat" and "cheese" in diet_restrictions:
+    print("Get a vegan pizza.")
 
-elif 'meat' in diet_restrictions:
-    print('Get a cheese pizza.')
+elif "meat" in diet_restrictions:
+    print("Get a cheese pizza.")
 
 else:
-    print('Get something else.')
+    print("Get something else.")
--- python-fundamentals-real-world/ch09-conditional-execution/specials_dictionary.py
+++ python-fundamentals-real-world/ch09-conditional-execution/specials_dictionary.py
@@ -1,18 +1,20 @@
 """ I'll Have the Special! """
 
-specials = {'Sunday': 'spinach',
-            'Monday': 'mushroom',
-            'Tuesday': 'pepperoni',
-            'Wednesday': 'veggie',
-            'Thursday': 'bbq chicken',
-            'Friday': 'sausage',
-            'Saturday': 'Hawaiian'}
+specials = {
+    "Sunday": "spinach",
+    "Monday": "mushroom",
+    "Tuesday": "pepperoni",
+    "Wednesday": "veggie",
+    "Thursday": "bbq chicken",
+    "Friday": "sausage",
+    "Saturday": "Hawaiian",
+}
 
 
 def order(day):
     pizza = specials[day]
-    print('Order the {} pizza.'.format(pizza))
+    print("Order the {} pizza.".format(pizza))
 
 
 # order the Saturday special!
-order('Saturday')
+order("Saturday")
