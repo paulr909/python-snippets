6b581a35e26644475cc34ee766d0b2cccccc9924
paulrogers <paulr909@proton.me>
2023-09-11 15:21:54 +0100

    revert pt2_class_inheritance.py

--- python-fundamentals-real-world/ch03-class-inheritance/pt2_class_inheritance.py
+++ python-fundamentals-real-world/ch03-class-inheritance/pt2_class_inheritance.py
@@ -41,9 +41,6 @@ class ECar(Car):  # Inherits from Car class
 my_ecar = ECar("white", "Nissan")
 my_ecar.window()
 my_ecar.radio()
-my_ecar.color()
-my_ecar.drive()
-my_ecar.drive()
 my_ecar.drive()
 
 # access the lingering gas tank
