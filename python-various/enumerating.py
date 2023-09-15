722689b25aea0c93e39999a742591486ebf4e484
paulrogers <paulr909@proton.me>
2023-09-15 15:24:22 +0100

    add enumerating.py

---
+++ python-various/enumerating.py
@@ -0,0 +1,19 @@
+# Enumerate over a list of items
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in enumerate(fruits):
+    print(fruit)
+print("\n")
+
+# Enumerate over a tuple of items with start index of 1
+colours = ("red", "green", "blue", "yellow", "orange", "purple")
+
+for index, colour in enumerate(colours, start=1):
+    print(index, colour)
+print("\n")
+
+# Enumerate over a dictionary
+person = {"name": "John", "age": 30, "city": "New York"}
+
+for index, (key, value) in enumerate(person.items()):
+    print(index, key, value)
