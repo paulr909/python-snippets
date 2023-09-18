11e1db0a12ec4496edce288c97bd4d778c08b1b7
paulrogers <paulr909@proton.me>
2023-09-18 17:44:22 +0100

    mv files into python-various

---
+++ python-various/loop_over-dict-and-list.py
@@ -0,0 +1,15 @@
+favorite_languages = {
+    "jen": "python",
+    "sarah": "c",
+    "edward": "ruby",
+    "phil": "python",
+}
+
+friends = ["phil", "sarah"]
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\n{name.title()}, I see you love {language}!")
