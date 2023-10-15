0b70958c4e8bd553a9bbdef953aa0c70faadd446
paulrogers <paulr909@proton.me>
2023-10-15 11:50:04 +0100

    add fibonacci.py

--- python-various/fibonacci.py
+++ python-various/fibonacci.py
@@ -1,15 +1,3 @@
-# FizzBuzz
-for num in range(1, 101):
-    if num % 5 == 0 and num % 3 == 0:
-        print("Fizz Buzz")
-    elif num % 3 == 0:
-        print("Fizz")
-    elif num % 5 == 0:
-        print("Buzz")
-    else:
-        print(num)
-
-
 # Fibonacci Sequence
 a, b = 0, 1
 for i in range(0, 10):
