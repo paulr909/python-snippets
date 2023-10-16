fd965642552fb79063ed096f2fff976dfefc12e6
paulrogers <paulr909@proton.me>
2023-10-16 09:54:44 +0100

    edit longest_even_length_word.py

--- python-various/longest_even_length_word.py
+++ python-various/longest_even_length_word.py
@@ -1,11 +1,13 @@
 import re
 
 text1 = "Start a private video call with your friends or colleagues!"
-text2 = "This is a simple test sentence with some words?"
+text2 = (
+    "This is a simple test sentence with some words? Plus some random numbers 3, 4, 6"
+)
 
 
 def longest_even_length_word(sentence):
-    strip_string = re.sub(r"[^a-zA-Z]", " ", sentence).split()
+    strip_string = re.sub(r"[^a-zA-Z0-9]", " ", sentence).split()
     print("Stripped:", " ".join(strip_string))
 
     even_length_words = [word for word in strip_string if len(word) % 2 == 0]
