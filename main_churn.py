647a7453958dff557e0236589adf3d589ee1d66b
paulrogers <paulr909@proton.me>
2023-12-09 15:36:31 +0000

    edit main_churn.py

--- main_churn.py
+++ main_churn.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 # import matplotlib.pyplot as plt
 
@@ -141,4 +142,57 @@ group = np.random.randint(10, size=6)
 
 # Insert at particular column
 df_new.insert(0, "Group", group)
+# print(df_new)
+
+# Where function
+# df_new["Balance"] = df_new["Balance"].where(df_new["Group"] >= 6, 0)
+# print(df_new)
+
+# Rank function
+# df_new["rank"] = df_new["Balance"].rank(method="first", ascending=False).astype("int")
+# print(df_new)
+
+# Number of unique values in a column
+# print(df.Geography.value_counts().size)
+
+# Number of unique values in a column
+# print(df.Geography.nunique())
+
+# Check memory usage
+# print(df.memory_usage(deep=True))
+
+# Category data type
+data = df["Geography"] = df["Geography"].astype("category")
+# print(data)
+
+# Replacing values
+df_new["Group"].replace(0, "B1")
+# print(df_new)
+
+# Replacing values with a dictionary
+df_new["Group"].replace({0: "B1", 1: "B2"}, inplace=True)
+# print(df_new)
+
+# Draw a histogram
+df["Balance"].plot(kind="hist", figsize=(10, 6), title="Customer Balance")
+# plt.show()
+
+# Reducing the decimal points of floats
+# df_new.round(2)
+# print(df_new)
+
+# Changing the display options
+pd.set_option("display.precision", 2)
+# print(df_new)
+
+# Calculating the percentage change through a column
+ser = pd.Series([4, 5, 2, 5, 6, 10, 12])
+# print(ser.pct_change())
+
+# Filtering based on strings
+start = df_new[df_new.Geography.str.startswith("Fr")]
+# print(start)
+
+# Styling a dataframe
+df_new.style.highlight_max(axis=0, color="darkgreen")
 print(df_new)
